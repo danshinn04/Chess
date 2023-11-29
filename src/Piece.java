a3b391bc4c6d618b82ac38f80f31a5d943445544
dansh04 <jshinn@bu.edu>
2023-11-29 16:39:41 -0500

    Step 1

--- src/Piece.java
+++ src/Piece.java
@@ -1,8 +1,10 @@
 import java.util.*;
-public interface Pieces {
+public interface Piece {
     Color getColor();
     PieceType getType();
     List<Move> getPossibleMoves(Board board, Position position);
     boolean move(Board board, Position newPosition);
-}
 
+    boolean getSymbol();
+
+}

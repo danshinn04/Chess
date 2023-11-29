947d533bc0c7c3ae4f53cebee9d48edae797e8cd
dansh04 <jshinn@bu.edu>
2023-11-29 16:31:32 -0500

    Class and interfaces

---
+++ src/PieceType.java
@@ -0,0 +1,2 @@
+public class PieceType {
+}
--- src/Pieces.java
+++ src/Pieces.java
@@ -1,2 +1,8 @@
+import java.util.*;
 public interface Pieces {
+    Color getColor();
+    PieceType getType();
+    List<Move> getPossibleMoves(Board board, Position position);
+    boolean move(Board board, Position newPosition);
 }
+
